294ca8ab343e2fd47f6563d18cbc78c66aec7e0d
Minhaz <sharifmdminhazurrahman@gmail.com>
2024-07-13 16:50:27 +0600

    config: update api baseURL

--- src/helpers/axios.ts
+++ src/helpers/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const fetcher = axios.create({
-	baseURL: "http://localhost:5000/api/v1",
 	// baseURL: "http://localhost:5000/api/v1",
+	baseURL: "https://next-bun-shop-backend.onrender.com/api/v1",
 	withCredentials: true,
 });
